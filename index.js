39361d71964718e541a4dc90ad497ccbb2d5dd0b
raisa-design <almeida.alves.raisa@gmail.com>
2021-11-09 15:09:06 -0300

    calculadora média

---
+++ index.js
@@ -0,0 +1,13 @@
+var nome="Raisa"
+var notaPrimeiroBimestre =9.7856
+var notaSegundoBimestre= 7.789
+var notaTerceiroBimestre= 4.566
+var notaQuartoBimestre= 2.56767
+var notaFinalDoBimestre=  (notaPrimeiroBimestre + notaSegundoBimestre +notaTerceiroBimestre + notaQuartoBimestre) / 4
+
+var notaFixada= notaFinalDoBimestre.toFixed(1)
+
+
+console.log("Bem vindo " + nome)
+console.log(notaPrimeiroBimestre)
+console.log(notaFixada)
